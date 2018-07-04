b1fa4771e1bbf44bc46e8d898af2c8f91b26e4f5
Thomas Pope <tpope0928@gmail.com>
2018-07-04 19:43:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,4 @@
 def display_board
   puts "Welcome to Tic Tack Toe!"
 end
-  array1.new = "   |   |   "
-  puts array1.new
-  array2.new = "-----------"
-  puts array2.new
-  puts array1.new
-  puts array2.new
-  puts array1.new
+
